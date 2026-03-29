dd6ec52b772f041b6fc7dcf7c0b4e6c7b3d4fe50
fabbasi25 <fabbasi@mit.edu>
2026-03-29 01:53:32 -0400

    adding new tests

---
+++ tests_p_1.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+#SBATCH --job-name=knapsack_p
+#SBATCH --output=logs/out_%A_%a.out
+#SBATCH --error=logs/err_%A_%a.err
+#SBATCH --array=0-17
+#SBATCH --time=15:00:00
+#SBATCH --cpus-per-task=1
+#SBATCH --mem=40G
+
+source /home/fnabbasi/projects/def-mh541-ab/fnabbasi/qiskit_env/bin/activate 
+
+OUTPUT_DIR="/home/fnabbasi/projects/def-mh541-ab/fnabbasi/knapsack-project/solutions/small_1_8192"
+
+mkdir -p $OUTPUT_DIR
+
+n=$((SLURM_ARRAY_TASK_ID + 1))
+OUTPUT_FILE="$OUTPUT_DIR/${n}-f.txt"
+
+echo "Running task $SLURM_ARRAY_TASK_ID (n=$n)"
+
+python tests_qaoa.py "$OUTPUT_FILE" "$n"
